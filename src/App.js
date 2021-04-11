462eb613a965b9b9c4efb30a2d34246a1cf45414
JulyB-creations <79759706+JulyB-creations@users.noreply.github.com>
2021-04-11 13:16:46 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">React Weather App</div>;
+  return (
+    <div className="App">
+      React Weather App
+      <footer>
+        This project was coded by J.B. and is{" "}
+        <a
+          href="https://github.com/JulyB-creations/weatherproject"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
