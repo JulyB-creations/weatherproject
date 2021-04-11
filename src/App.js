f5ccca867ed9339c9c7671a36cf7cd89ec6bed69
JulyB-creations <79759706+JulyB-creations@users.noreply.github.com>
2021-04-11 13:33:23 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      React Weather App
-      <footer>
-        This project was coded by J.B. and is{" "}
-        <a
-          href="https://github.com/JulyB-creations/weatherproject"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        React Weather App
+        <footer>
+          This project was coded by J.B. and is{" "}
+          <a
+            href="https://github.com/JulyB-creations/weatherproject"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
