241a1b3a0267a34364a7d006eecd965fe9d58a6c
JulyB-creations <79759706+JulyB-creations@users.noreply.github.com>
2021-04-11 13:56:10 +0200

    weather.js basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        React Weather App
+        <Weather />
         <footer>
           This project was coded by J.B. and is{" "}
           <a
